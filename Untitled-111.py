5c65d27a9687d09c39138c43d9e043a7962f246f
Алексей Жигалов <avzh79@gmail.com>
2021-12-02 19:31:30 +0300

    aaaa commit

--- Untitled-111.py
+++ Untitled-111.py
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 from math import sqrt
 
 message = (
@@ -23,26 +23,4 @@ def calc(your_number):
 print(message)
 calc(25.5)
 
-=======
-from math import *
-import itertools
 
-
-message = 'Добро пожаловать в самую лучшую программу для вычисления ' \
-          'квадратного корня из заданного числа'
-
-def  CalculateSquareRoot (Number ):
-    """ Вычисляет квадратный корень"""
-    return  sqrt(Number )
-
-def calc(your_number) :
-    if your_number<=0:
-        return    
-     
-    root = 0
-    print(f"Мы вычислили корень квадратный из введенного вами числа. Это будет: {CalculateSquareRoot(your_number)}")
-
-
-print (message)
-calc (25.5)
->>>>>>> 09af60d6ea093a64fb3549ef1b93b6988956a567
