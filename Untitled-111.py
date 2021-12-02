3d5a472c4321b5a40342a87227d0b44002dce198
Алексей Жигалов <avzh79@gmail.com>
2021-12-02 18:01:20 +0300

    From work 2

--- Untitled-111.py
+++ Untitled-111.py
@@ -1,21 +1,24 @@
 from math import sqrt
-import itertools
+
+message = (
+            'Добро пожаловать в самую лучшую программу для '
+            'вычисления квадратного корня из заданного числа'
+)
 
 
-message = 'Добро пожаловать в самую лучшую программу для вычисления ' \
-          'квадратного корня из заданного числа'
-
-def  CalculateSquareRoot (Number ):
-    """ Вычисляет квадратный корень"""
-    return  sqrt(Number )
-
-def calc(your_number) :
-    if your_number<=0:
-        return    
-     
-    root = 0
-    print(f"Мы вычислили корень квадратный из введенного вами числа. Это будет: {CalculateSquareRoot(your_number)}")
+def calculate_square_root(number):
+    """Вычисляет квадратный корень."""
+    return sqrt(number)
 
 
-print (message)
-calc (25.5)
+def calc(your_number):
+    """Формирует результат."""
+    if your_number >= 0:
+        root = calculate_square_root(your_number)
+        print(f'Мы вычислили корень квадратный из введенного вами числа.'
+              f' Это будет: {root}')
+
+
+print(message)
+calc(25.5)
+
